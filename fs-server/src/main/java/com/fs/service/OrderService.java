3af77e92560660fb3fb291df65619ed58e3d067e
JesusMan0529 <1638272030@qq.com>
2026-07-17 20:44:23 +0800

    Customer Order Reminder

--- fs-server/src/main/java/com/fs/service/OrderService.java
+++ fs-server/src/main/java/com/fs/service/OrderService.java
@@ -95,4 +95,11 @@ public interface OrderService {
      * @param id
      */
     void complete(Long id);
+
+    /**
+     * 客户催单
+     *
+     * @param id
+     */
+    void reminder(Long id);
 }
--- fs-server/src/main/java/com/fs/service/impl/OrderServiceImpl.java
+++ fs-server/src/main/java/com/fs/service/impl/OrderServiceImpl.java
@@ -162,6 +162,7 @@ public class OrderServiceImpl implements OrderService {
                 .build();
         orderMapper.update(orders);
 
+        //来单提醒
         //通过WebSocket向商家端推送消息 type orderId content
         Map map = new HashMap();
         map.put("type", 1); // 1表示来单提醒 2表示客户催单
@@ -478,6 +479,28 @@ public class OrderServiceImpl implements OrderService {
         orders.setDeliveryTime(LocalDateTime.now());
 
         orderMapper.update(orders);
+    }
+
+    /***
+     * 客户催单
+     * @param id
+     */
+    public void reminder(Long id) {
+        // 根据id查询订单
+        Orders ordersDB = orderMapper.getById(id);
+
+        // 校验订单是否存在
+        if (ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        Map map = new HashMap();
+        map.put("type", 2); // 1表示来单提醒 2表示客户催单
+        map.put("orderId",ordersDB.getId());
+        map.put("content", "订单号：" + ordersDB.getNumber());
+
+        //通过websocket向客户端浏览器推送消息
+        webSocketServer.sendToAllClient(JSON.toJSONString(map));
     }
 
     /**
