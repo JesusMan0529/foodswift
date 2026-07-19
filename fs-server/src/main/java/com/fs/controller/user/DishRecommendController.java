5b804fb8f9fe816f5958c9c53e258eef46f92501
QuGuyu <132816578+derser360@users.noreply.github.com>
2026-07-19 15:30:38 +0800

    Comments optimized and DishRecommendController now moved to the right directory (#3)

---
+++ fs-server/src/main/java/com/fs/controller/user/DishRecommendController.java
@@ -0,0 +1,40 @@
+package com.fs.controller.user;
+
+import com.fs.context.BaseContext;
+import com.fs.entity.Dish;
+import com.fs.result.Result;
+import com.fs.task.DishRecommendTask;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 每日推荐菜品接口
+ */
+@RestController
+@RequestMapping("/user/dishRecommend")
+@Api(tags = "C端-每日推荐菜品接口")
+public class DishRecommendController {
+
+    @Autowired
+    private DishRecommendTask dishRecommendTask;
+
+    /**
+     * 查询今日推荐的三道随机菜品
+     * 仅当店铺在当前登录用户收货地址的配送范围内时才会返回推荐
+     * 地图服务异常或未设置地址时降级为正常推荐
+     *
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation("查询今日推荐的三道随机菜品")
+    public Result<List<Dish>> list() {
+        List<Dish> list = dishRecommendTask.getTodayRecommendDishes(BaseContext.getCurrentId());
+        return Result.success(list);
+    }
+}
